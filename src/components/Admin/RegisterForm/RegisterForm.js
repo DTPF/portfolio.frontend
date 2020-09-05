07ff236b42dab9588477b39c36118a7427bab737
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-05 19:43:40 +0200

    Adding hook for RegisterForm

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Button, Checkbox, notification } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 import "./RegisterForm.scss";
 
 export default function RegisterForm() {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+    privacyPolicy: false,
+  });
+
+  const changeForm = (e) => {
+    if (e.target.name === "privacyPolicy") {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.checked,
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const register = e => {
+    console.log(inputs);
+  };
+
   return (
-    <Form className="register-form">
+    <Form className="register-form" onFinish={register} onChange={changeForm}>
       <Form.Item>
         <Input
           prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
@@ -14,6 +39,7 @@ export default function RegisterForm() {
           name="email"
           placeholder="Correo electrónico"
           className="register-form__input"
+          value={inputs.email}
         />
       </Form.Item>
       <Form.Item>
@@ -23,6 +49,7 @@ export default function RegisterForm() {
           name="password"
           placeholder="Contraseña"
           className="register-form__input"
+          value={inputs.password}
         />
       </Form.Item>
       <Form.Item>
@@ -32,17 +59,18 @@ export default function RegisterForm() {
           name="repeatPassword"
           placeholder="Repetir contraseña"
           className="register-form__input"
+          value={inputs.repeatPassword}
         />
       </Form.Item>
       <Form.Item>
-          <Checkbox name="privacyPolicy">
-              He leído y acepto la política de privacidad.
-          </Checkbox>
+        <Checkbox name="privacyPolicy" checked={inputs.privacyPolicy}>
+          He leído y acepto la política de privacidad.
+        </Checkbox>
       </Form.Item>
       <Form.Item>
-          <Button htmlType="submit" className="register-form__button">
-              Crear cuenta
-          </Button>
+        <Button htmlType="submit" className="register-form__button">
+          Crear cuenta
+        </Button>
       </Form.Item>
     </Form>
   );
