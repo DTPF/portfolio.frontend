5b8d0bc983de1784e934c57ca8797b8a41ec0261
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-07 14:28:40 +0200

    Creating the functions to get AccessToken and RefreshToken

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -52,7 +52,6 @@ export default function RegisterForm() {
   };
 
   const register = async (e) => {
-    // const { email, password, repeatPassword, privacyPolicy } = formValid;
     const emailVal = inputs.email;
     const passwordVal = inputs.password;
     const repeatPasswordVal = inputs.repeatPassword;
