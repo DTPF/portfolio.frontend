1ef9a694f64b662b9e7bcc716fc160110a1d241d
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-08 11:49:47 +0200

    Getting users

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAccessTokenApi } from "../../../api/auth";
+import { getUsersApi } from "../../../api/user";
 
 import "./Users.scss";
 
 export default function Users() {
-    return (
-        <div>
-            <h1>Lista de usuarios</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  const token = getAccessTokenApi();
+
+  console.log(users);
+
+  useEffect(() => {
+    getUsersApi(token).then((response) => {
+      setUsers(response);
+    });
+  }, [token]);
+  return (
+    <div>
+      <h1>Lista de usuarios</h1>
+    </div>
+  );
 }
