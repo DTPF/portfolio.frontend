69b30100f24330b3f09aaeb760ad79665ec092ed
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-08 12:34:09 +0200

    Getting active users

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { getAccessTokenApi } from "../../../api/auth";
-import { getUsersApi } from "../../../api/user";
+import { getUsersActiveApi } from "../../../api/user";
 
 import "./Users.scss";
 
 export default function Users() {
-  const [users, setUsers] = useState([]);
+  const [usersActive, setUsersActive] = useState([]);
+  const [usersInactive, setUsersInactive] = useState([]);
   const token = getAccessTokenApi();
 
-  console.log(users);
+  console.log('usersActive:', usersActive);
+  console.log('usersInactive:', usersInactive);
 
   useEffect(() => {
-    getUsersApi(token).then((response) => {
-      setUsers(response);
+    getUsersActiveApi(token, true).then((response) => {
+      setUsersActive(response);
+    });
+    getUsersActiveApi(token, true).then((response) => {
+      setUsersInactive(response);
     });
   }, [token]);
+
   return (
     <div>
       <h1>Lista de usuarios</h1>
