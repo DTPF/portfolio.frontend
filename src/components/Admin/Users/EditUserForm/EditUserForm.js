37b016d51a6a92ecea6442e10f42ba7213bb2618
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-09 12:30:36 +0200

    Adding user edit form

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -1,20 +1,40 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Avatar, Form, Icon, Input, Select, Button, Row, Col } from "antd";
 import { useDropzone } from "react-dropzone";
 import NoAvatar from "../../../../assets/img/png/no-avatar.png";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 import "./EditUserForm.scss";
 
 export default function EditUserForm(props) {
   const { user } = props;
   const [avatar, setAvatar] = useState(null);
+  const [userData, setUserData] = useState({
+    name: user.name,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role,
+    avatar: user.avatar,
+  });
 
-  console.log(avatar);
+  useEffect(() => {
+    if (avatar) {
+      setUserData({ ...userData, avatar });
+    }
+  }, [avatar]);
+
+  const updateUser = (e) => {
+    console.log(userData);
+  };
 
   return (
     <div className="edit-user-form">
       <UploadAvatar avatar={avatar} setAvatar={setAvatar} />
-      <h2>{user.email}</h2>
+      <EditForm
+        userData={userData}
+        setUserData={setUserData}
+        updateUser={updateUser}
+      />
     </div>
   );
 }
@@ -45,5 +65,99 @@ function UploadAvatar(props) {
         <Avatar size={150} src={avatar ? avatar.preview : NoAvatar} />
       )}
     </div>
+  );
+}
+
+function EditForm(props) {
+  const { userData, setUserData, updateUser } = props;
+  const { Option } = Select;
+
+  return (
+    <Form className="form-edit" onFinish={updateUser}>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Nombre"
+              defaultValue={userData.name}
+              onChange={(e) =>
+                setUserData({ ...userData, name: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Apellidos"
+              defaultValue={userData.lastName}
+              onChange={(e) =>
+                setUserData({ ...userData, lastName: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Correo electrónico"
+              defaultValue={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Select
+              placeholder="Selecciona un rol"
+              onChange={(e) => setUserData({ ...userData, role: e })}
+              defaultValue={userData.role}
+            >
+              <Option value="admin">Administrador</Option>
+              <Option value="editor">Editor</Option>
+              <Option value="revisor">Revisor</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Contraseña"
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Repetir Contraseña"
+              onChange={(e) =>
+                setUserData({ ...userData, repeatPassword: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="btn-submit">
+          Actualizar Usuario
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
