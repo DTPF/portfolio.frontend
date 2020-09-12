6856da61551784968194a4defdd80597ba00eaaf
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-12 13:05:18 +0200

    Updating menu order in the database

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Switch, List, Button, Modal as ModalAntd, notification } from "antd";
+import { Switch, List, Button, Modal as ModalAntd, notification, message } from "antd";
 import Modal from "../../../Modal";
 import DragSortableList from "react-drag-sortable";
+import { updateMenuApi } from "../../../../api/menu";
+import { getAccessTokenApi } from "../../../../api/auth";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 import "./MenuWebList.scss";
@@ -23,14 +25,20 @@ export default function MenuWebList(props) {
       listItemsArray.push({
         content: (
           <MenuItem item={item} />
-        ),
+        )
       });
     });
     setListItems(listItemsArray);
   }, [menu]);
 
   const onSort = (sortedList, dropEvent) => {
-    console.log(sortedList);
+    const accessToken = getAccessTokenApi();
+
+    sortedList.forEach(item => {
+      const { _id } = item.content.props.item;
+      const order = item.rank;
+      updateMenuApi(accessToken, _id, { order });
+    });
   };
   return (
     <div className="menu-web-list">
