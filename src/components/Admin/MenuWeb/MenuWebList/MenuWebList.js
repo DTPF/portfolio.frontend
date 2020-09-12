64df3ca681e00715007b90bd1a5839a8d141fce0
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-12 13:51:55 +0200

    Adding function to activate and deactivate menus

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Switch, List, Button, Modal as ModalAntd, notification, message } from "antd";
 import Modal from "../../../Modal";
 import DragSortableList from "react-drag-sortable";
-import { updateMenuApi } from "../../../../api/menu";
+import { updateMenuApi, activateMenuApi } from "../../../../api/menu";
 import { getAccessTokenApi } from "../../../../api/auth";
+import { notifDelay, notifDelayErr } from "../../../../config/notifications";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 import "./MenuWebList.scss";
@@ -24,12 +25,32 @@ export default function MenuWebList(props) {
     menu.forEach((item) => {
       listItemsArray.push({
         content: (
-          <MenuItem item={item} />
+          <MenuItem item={item} activateMenu={activateMenu} />
         )
       });
     });
     setListItems(listItemsArray);
   }, [menu]);
+
+  const activateMenu = (menu, status) => {
+    const accessToken = getAccessTokenApi();
+    
+    activateMenuApi(accessToken, menu._id, status)
+    .then(response => {
+      console.log(status);
+        if(status === true) {
+          notification["success"]({
+            message: response.message,
+            duration: notifDelay
+          });
+        } else {
+          notification["warning"]({
+            message: response.message,
+            duration: notifDelayErr
+          });
+        }
+      });
+  };
 
   const onSort = (sortedList, dropEvent) => {
     const accessToken = getAccessTokenApi();
@@ -40,6 +61,7 @@ export default function MenuWebList(props) {
       updateMenuApi(accessToken, _id, { order });
     });
   };
+
   return (
     <div className="menu-web-list">
       <div className="menu-web-list__header">
@@ -53,12 +75,12 @@ export default function MenuWebList(props) {
 }
 
 function MenuItem(props) {
-  const { item } = props;
+  const { item, activateMenu } = props;
 
   return (
     <List.Item
       actions={[
-        <Switch defaultChecked={item.active} />,
+        <Switch defaultChecked={item.active} onChange={e => activateMenu(item, e)} />,
         <Button type="primary">
           <EditOutlined />
         </Button>,
