ce8ba4808a49c7dd372da8735fe9854056c9ebc2
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-01 16:34:40 +0100

    Added checkConnection in useConnection and added error to LayoutBasic

--- src/hooks/useConnection.js
+++ src/hooks/useConnection.js
@@ -4,6 +4,7 @@ import { message } from "antd";
 
 export default function useConnection() {
   const [connection, setConnection] = useState(null);
+  const [checkConnection, setCheckConnection] = useState(null);
   useEffect(() => {
     let unmounted = false;
     connectionApi().then((response) => {
@@ -16,12 +17,32 @@ export default function useConnection() {
           setConnection(500);
           message.error(
             "Ha ocurrido un error en el servidor, vuelve más tarde y disculpa las molestias.",
-            20
+            10
           );
         }
       }
     });
     return () => { unmounted = true };
   }, []);
-  return connection;
+
+  useEffect(() => {
+    let unmounted = false;
+    setInterval(() => {
+      connectionApi().then((response) => {
+        if (response.status) {
+          if (!unmounted) {
+            setCheckConnection(response.status);
+            setConnection(response.status);
+          }
+        } else {
+          if (!unmounted) {
+            setCheckConnection(500);
+          }
+        }
+      });
+    }, 3000);
+    return () => { unmounted = true };
+  }, []);
+
+  return { connection, checkConnection };
 }
--- src/layouts/LayoutBasic.tsx
+++ src/layouts/LayoutBasic.tsx
@@ -13,9 +13,42 @@ const Error = lazy(() => import("../pages/Errors"));
 
 export default function LayoutBasic(props: any) {
   const { routes } = props;
-  const connection = useConnection();
+  const { connection, checkConnection } = useConnection();
   const [menuCollapsed, setMenuCollapsed] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  if (checkConnection === 500) {
+    return (
+      <RenderLayoutBasicError
+        routes={routes}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        menuCollapsed={menuCollapsed}
+        setMenuCollapsed={setMenuCollapsed}
+        connection={connection}
+      />
+    );
+  }
+  return (
+    <RenderLayoutBasic
+      routes={routes}
+      isLoading={isLoading}
+      setIsLoading={setIsLoading}
+      menuCollapsed={menuCollapsed}
+      setMenuCollapsed={setMenuCollapsed}
+      connection={connection}
+    />
+  );
+}
+
+function RenderLayoutBasic(props: any) {
+  const {
+    routes,
+    isLoading,
+    setIsLoading,
+    menuCollapsed,
+    setMenuCollapsed,
+    connection,
+  } = props;
   const backgroundImage = window.innerWidth >= 650 ? desktopImage : mobileImage;
   const { Content } = Layout;
   const closeMenu = () => {
@@ -44,17 +77,56 @@ export default function LayoutBasic(props: any) {
         <Content className="layout-basic__content">
           {!connection ? (
             <Spin />
-            ) : (
-              <>
+          ) : (
+            <>
               {connection === 200 ? (
-                <>
-              <LoadRoutes routes={routes && routes} />  
-              </>
+                <LoadRoutes routes={routes && routes} />
               ) : (
                 <Error status={500} />
-                )}
+              )}
             </>
           )}
+        </Content>
+        <div className="layout-basic__footer">{isLoading && <Footer />}</div>
+      </Suspense>
+    </div>
+  );
+}
+
+function RenderLayoutBasicError(props: any) {
+  const {
+    isLoading,
+    setIsLoading,
+    menuCollapsed,
+    setMenuCollapsed
+  } = props;
+  const backgroundImage = window.innerWidth >= 650 ? desktopImage : mobileImage;
+  const { Content } = Layout;
+  const closeMenu = () => {
+    if (menuCollapsed === false) {
+      setMenuCollapsed(true);
+    }
+  };
+  return (
+    <div
+      className="layout-basic"
+      onClick={closeMenu}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Suspense fallback={<></>}>
+        <div className="layout-basic__header">
+          <MenuTop
+            menuCollapsed={menuCollapsed}
+            setMenuCollapsed={setMenuCollapsed}
+            setIsLoading={setIsLoading}
+          />
+          <MenuSider
+            menuCollapsed={menuCollapsed}
+            setMenuCollapsed={setMenuCollapsed}
+          />
+        </div>
+        <Content className="layout-basic__content">
+          <Error status={500} />
         </Content>
         <div className="layout-basic__footer">{isLoading && <Footer />}</div>
       </Suspense>
