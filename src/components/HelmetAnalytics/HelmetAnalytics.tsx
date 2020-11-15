7b77984b430ffb0828709a50a6eff8790dc64344
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-15 21:57:27 +0100

    Google Analytics implementation refactored

--- src/components/HelmetAnalytics/HelmetAnalytics.tsx
+++ src/components/HelmetAnalytics/HelmetAnalytics.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import { Helmet } from "react-helmet";
 import { Cookies } from "react-cookie";
+import gtag from "../../utils/gtag";
 const CookiesConsent = lazy(() => import("../UI/CookiesConsent"));
 
 export default function HelmetAnalytics(props: {
@@ -8,8 +9,12 @@ export default function HelmetAnalytics(props: {
   contentHelmet: string;
 }) {
   const { titleHelmet, contentHelmet } = props;
-  const cookies = new Cookies();
-  const _gaCookies = cookies.get("_gaCookies");  
+  const cookie = new Cookies();
+  const _gaCookies = cookie.get("_gaCookies");
+  const tagInit = () => {
+    gtag("js", new Date());
+    gtag("config", "G-66JXXM1GXF");
+  };
   return (
     <>
       {!_gaCookies ? (
@@ -34,7 +39,11 @@ export default function HelmetAnalytics(props: {
             content={contentHelmet}
             data-react-helmet="true"
           />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-66JXXM1GXF"></script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-66JXXM1GXF"
+          ></script>
+          {tagInit()}
         </Helmet>
       )}
     </>
