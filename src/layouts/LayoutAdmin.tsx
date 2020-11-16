1dc39201e7e18d4a02f7f66572b58a8c9f927f51
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-16 19:29:11 +0100

    Message added when connection fails

--- src/layouts/LayoutAdmin.tsx
+++ src/layouts/LayoutAdmin.tsx
@@ -1,20 +1,20 @@
 import React, { useState, Suspense, lazy } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { Layout } from "antd";
+import { Layout, Tag } from "antd";
 import useAuth from "../hooks/useAuth";
 import useConnection from "../hooks/useConnection";
 import AdminSignIn from "../pages/Admin/SignIn";
 import { Notifications } from "react-push-notification";
+import { StopOutlined } from "@ant-design/icons";
 import "./LayoutAdmin.scss";
 const MenuTop = lazy(() => import("../components/Admin/MenuTop"));
 const MenuSider = lazy(() => import("../components/Admin/MenuSider"));
 const LoadRoutes = lazy(() => import("../providers/LoadRoutes"));
-const Spin = lazy(() => import("../components/UI/Spin"));
 const Error = lazy(() => import("../pages/Errors"));
 
 export default function LayoutAdmin(props: any) {
   const { routes } = props;
-  const { connection, checkConnection } = useConnection();
+  const { connection, isOnline } = useConnection();
   const { user, isLoading } = useAuth();
   if (!user && !isLoading) {
     return (
@@ -25,10 +25,16 @@ export default function LayoutAdmin(props: any) {
     );
   }
   if (user && !isLoading) {
-    if (checkConnection === 500) {
-      return <RenderLayoutAdminError />
-    }
-    return <RenderLayoutAdmin routes={routes} connection={connection} />
+    return (
+      <>
+        {!isOnline && (
+          <Tag className="offline-message" icon={<StopOutlined />}>
+            Offline
+          </Tag>
+        )}
+        <RenderLayoutAdmin routes={routes} connection={connection} />
+      </>
+    );
   }
   return null;
 }
@@ -47,9 +53,7 @@ function RenderLayoutAdmin(props: any) {
       <Notifications />
       <Suspense fallback={<></>}>
         <MenuSider menuCollapsed={menuCollapsed} />
-        <Layout
-          className="layout-admin"
-        >
+        <Layout className="layout-admin">
           <Header className="layout-admin__header">
             <MenuTop
               menuCollapsed={menuCollapsed}
@@ -57,52 +61,11 @@ function RenderLayoutAdmin(props: any) {
             />
           </Header>
           <Content className="layout-admin__content">
-            {!connection ? (
-              <Spin />
+            {connection !== 500 ? (
+              <LoadRoutes routes={routes && routes} />
             ) : (
-              <>
-                {connection === 200 ? (
-                  <LoadRoutes routes={routes && routes} />
-                ) : (
-                  <Error status={500} />
-                )}
-              </>
+              <Error status={500} />
             )}
-          </Content>
-          <Footer className="layout-admin__footer">
-            David Thomas Pizarro Frick
-          </Footer>
-        </Layout>
-      </Suspense>
-    </Layout>
-  );
-}
-
-function RenderLayoutAdminError(props: any) {
-  const [menuCollapsed, setMenuCollapsed] = useState(false);
-  const { Header, Content, Footer } = Layout;
-  const closeMenu = () => {
-    if (menuCollapsed === false) {
-      setMenuCollapsed(true);
-    }
-  };
-  return (
-    <Layout onClick={closeMenu}>
-      <Notifications />
-      <Suspense fallback={<></>}>
-        <MenuSider menuCollapsed={menuCollapsed} />
-        <Layout
-          className="layout-admin"
-          style={{ marginLeft: menuCollapsed ? "0px" : "205px" }}
-        >
-          <Header className="layout-admin__header">
-            <MenuTop
-              menuCollapsed={menuCollapsed}
-              setMenuCollapsed={setMenuCollapsed}
-            />
-          </Header>
-          <Content className="layout-admin__content">
-            <Error status={500} />
           </Content>
           <Footer className="layout-admin__footer">
             David Thomas Pizarro Frick
--- src/layouts/LayoutBasic.tsx
+++ src/layouts/LayoutBasic.tsx
@@ -1,7 +1,8 @@
 import React, { useState, Suspense, lazy } from "react";
 import useConnection from "../hooks/useConnection";
 import { gaEvent } from "../utils/analytics.js";
-import { Layout, BackTop } from "antd";
+import { Layout, BackTop, Tag, Alert } from "antd";
+import { StopOutlined } from "@ant-design/icons";
 import "./LayoutBasic.scss";
 const desktopImage = require("../assets/img/jpg/background-squares.jpg");
 const mobileImage = require("../assets/img/jpg/background-squares-mobile.jpg");
@@ -9,27 +10,28 @@ const MenuTop = lazy(() => import("../components/Web/Layout/MenuTop"));
 const MenuSider = lazy(() => import("../components/Web/Layout/MenuSider"));
 const Footer = lazy(() => import("../components/Web/Layout/Footer"));
 const LoadRoutes = lazy(() => import("../providers/LoadRoutes"));
-const Spin = lazy(() => import("../components/UI/Spin"));
 const Error = lazy(() => import("../pages/Errors"));
 
 export default function LayoutBasic(props: any) {
   const { routes } = props;
-  const { connection, checkConnection } = useConnection();
+  const { connection, isOnline } = useConnection();
   const [menuCollapsed, setMenuCollapsed] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  if (checkConnection === 500) {
-    return (
-      <RenderLayoutBasicError
-        routes={routes}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-        menuCollapsed={menuCollapsed}
-        setMenuCollapsed={setMenuCollapsed}
-        connection={connection}
-      />
-    );
-  }
   return (
+    <>
+      {!isOnline && (
+        <>
+          <Tag className="offline-message" icon={<StopOutlined />}>
+            Offline
+          </Tag>
+          <Alert
+            className="offline-message-alert"
+            message="Comprueba tu conexión a internet para disfrutar de la web al completo."
+            type="info"
+            closeText="Cerrar aviso"
+          />
+        </>
+      )}
       <RenderLayoutBasic
         routes={routes}
         isLoading={isLoading}
@@ -38,6 +40,7 @@ export default function LayoutBasic(props: any) {
         setMenuCollapsed={setMenuCollapsed}
         connection={connection}
       />
+    </>
   );
 }
 
@@ -66,7 +69,7 @@ function RenderLayoutBasic(props: any) {
       onClick={closeMenu}
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <BackTop duration={600} onClick={clickBackTop}/>
+      <BackTop duration={600} onClick={clickBackTop} />
       <Suspense fallback={<></>}>
         <div className="layout-basic__header">
           <MenuTop
@@ -80,58 +83,11 @@ function RenderLayoutBasic(props: any) {
           />
         </div>
         <Content className="layout-basic__content">
-          {!connection ? (
-            <Spin />
+          {connection !== 500 ? (
+            <LoadRoutes routes={routes && routes} />
           ) : (
-            <>
-              {connection === 200 ? (
-                <LoadRoutes routes={routes && routes} />
-              ) : (
-                <Error status={500} />
-              )}
-            </>
+            <Error status={500} />
           )}
-        </Content>
-        <div className="layout-basic__footer">{isLoading && <Footer />}</div>
-      </Suspense>
-    </div>
-  );
-}
-
-function RenderLayoutBasicError(props: any) {
-  const {
-    isLoading,
-    setIsLoading,
-    menuCollapsed,
-    setMenuCollapsed
-  } = props;
-  const backgroundImage = window.innerWidth >= 650 ? desktopImage : mobileImage;
-  const { Content } = Layout;
-  const closeMenu = () => {
-    if (menuCollapsed === false) {
-      setMenuCollapsed(true);
-    }
-  };
-  return (
-    <div
-      className="layout-basic"
-      onClick={closeMenu}
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <Suspense fallback={<></>}>
-        <div className="layout-basic__header">
-          <MenuTop
-            menuCollapsed={menuCollapsed}
-            setMenuCollapsed={setMenuCollapsed}
-            setIsLoading={setIsLoading}
-          />
-          <MenuSider
-            menuCollapsed={menuCollapsed}
-            setMenuCollapsed={setMenuCollapsed}
-          />
-        </div>
-        <Content className="layout-basic__content">
-          <Error status={500} />
         </Content>
         <div className="layout-basic__footer">{isLoading && <Footer />}</div>
       </Suspense>
