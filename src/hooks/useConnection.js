1802e3dab0d7c07492818b60be41aea865122118
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-16 19:19:57 +0100

    useConnection hook improved

--- src/hooks/useConnection.js
+++ src/hooks/useConnection.js
@@ -4,47 +4,71 @@ import { message } from "antd";
 
 export default function useConnection() {
   const [connection, setConnection] = useState(null);
-  const [checkConnection, setCheckConnection] = useState(null);
+  const [isOnline, setIsOnline] = useState(true);
   const [timeToReload, setTimeToReload] = useState(1000);
   useEffect(() => {
     let unmounted = false;
-    connectionApi().then((response) => {
-      if (response.status) {
-        if (!unmounted) {
-          setConnection(response.status);
-        }
-      } else {
-        if (!unmounted) {
-          setConnection(500);
-          message.error(
-            "Ha ocurrido un error en el servidor, vuelve más tarde y disculpa las molestias.",
-            5
-          );
-        }
+    let onLine = window.navigator.onLine;
+    if (onLine) {
+      if (!unmounted) {
+        setIsOnline(onLine);
       }
-    });
+      connectionApi().then((response) => {
+        if (response.status) {
+          if (!unmounted) {
+            setConnection(response.status);
+          }
+        } else {
+          if (!unmounted) {
+            setConnection(500);
+            message.error(
+              "Ha ocurrido un error en el servidor, vuelve más tarde y disculpa las molestias.",
+              5
+            );
+          }
+        }
+      });
+    } else {
+      if (!unmounted) {
+        setConnection(200);
+        setIsOnline(onLine);
+      }
+    }
     return () => { unmounted = true };
   }, []);
   useEffect(() => {
     let unmounted = false;
     const interval = setInterval(() => {
-      connectionApi().then((response) => {
-        if (response.status) {
-          if (!unmounted) {
-            setCheckConnection(response.status);
-            setConnection(response.status);
-            setTimeToReload(30000);
-          }
-        } else {
-          if (!unmounted) {
-            setCheckConnection(500);
-            setConnection(500);
-            setTimeToReload(1000);
-          }
+      let onLine = window.navigator.onLine;
+      if (onLine) {
+        if (!unmounted) {
+          setIsOnline(onLine);
         }
-      });
+        connectionApi().then((response) => {
+          if (response.status) {
+            if (!unmounted) {
+              setConnection(response.status);
+              setTimeToReload(30000);
+            }
+          } else {
+            if (!unmounted) {
+              setConnection(500);
+              setTimeToReload(1000);
+            }
+          }
+        });
+      } else {
+        if (!unmounted) {
+          setConnection(200);
+          setIsOnline(onLine);
+          setTimeToReload(1000);
+        }
+      }
     }, timeToReload);
-    return () => { clearInterval(interval); unmounted = true };
-  }, [timeToReload]);
-  return { connection, checkConnection };
+    return () => {
+      clearInterval(interval);
+      unmounted = true;
+    };
+  }, [timeToReload, isOnline]);
+  return { connection, isOnline };
 }
