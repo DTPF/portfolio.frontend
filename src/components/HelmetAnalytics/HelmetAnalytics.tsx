6eed4f771d5357e8bf3c302bf490b43c5b4c68d5
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-17 17:00:22 +0100

    Gtag implementation fixed

--- src/components/HelmetAnalytics/HelmetAnalytics.tsx
+++ src/components/HelmetAnalytics/HelmetAnalytics.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { Helmet } from "react-helmet";
 import { Cookies } from "react-cookie";
-import gtag from "../../utils/gtag";
 const CookiesConsent = lazy(() => import("../UI/CookiesConsent"));
 
 export default function HelmetAnalytics(props: {
@@ -11,10 +10,6 @@ export default function HelmetAnalytics(props: {
   const { titleHelmet, contentHelmet } = props;
   const cookie = new Cookies();
   const _gaCookies = cookie.get("_gaCookies");
-  const tagInit = () => {
-    gtag("js", new Date());
-    gtag("config", "G-66JXXM1GXF");
-  };
   return (
     <>
       {!_gaCookies ? (
@@ -43,7 +38,6 @@ export default function HelmetAnalytics(props: {
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-66JXXM1GXF"
           ></script>
-          {tagInit()}
         </Helmet>
       )}
     </>
--- src/utils/gtag.jsx
+++ src/utils/gtag.jsx
@@ -2,3 +2,5 @@ window.dataLayer = window.dataLayer || [];
 export default function gtag() {
   window.dataLayer.push(arguments);
 }
+gtag("js", new Date());
+gtag("config", "G-66JXXM1GXF");
