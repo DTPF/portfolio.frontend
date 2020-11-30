713933469c45f84bb667087322fe75005118f236
David Pizarro <davidpizarrofrick@gmail.com>
2020-11-30 17:10:06 +0100

    conts added

--- src/config/url.js
+++ src/config/url.js
@@ -3,3 +3,4 @@ export const ENDPOINT = "http://localhost:3998";
 // export const URL = "https://dtpf.es/";
 // export const ENDPOINT = "https://dtpf.es";
 // export const URL = "https://beta.dtpf.es/";
+// export const ENDPOINT = "https://beta.dtpf.es";
