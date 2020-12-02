563cbcb21b4d2b5fdaa3e82c0690edac918997e7
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-02 20:17:37 +0100

    Tests added to api/auth

--- src/__tests__/api/auth.test.js
+++ src/__tests__/api/auth.test.js
@@ -1,12 +1,25 @@
 import { testingRefreshToken } from "../../utils/constants";
-import { willExpireToken } from "../../api/auth";
+import { getAccessTokenApi, refreshAccessTokenApi, logout, willExpireToken } from "../../api/auth";
 
 const TOKEN = testingRefreshToken;
 
 describe("Api de auth", () => {
-    it("Comprobar si ha expirado el token", (done) => {
-        const data = willExpireToken(TOKEN);
-        expect(data).toBeFalsy();
-        done();
-    });
+  it("Comprobar si ha expirado el token", (done) => {
+    const data = willExpireToken(TOKEN);
+    expect(data).toBeFalsy();
+    done();
+  });
+  it("getAccessTokenApi", (done) => {
+    expect(getAccessTokenApi()).toBe(null);
+    done();
+  });
+  it("refreshAccessTokenApi", (done) => {
+    expect(refreshAccessTokenApi(TOKEN)).toBe(undefined);
+    done();
+  });
+  it("logout", (done) => {
+    expect(logout()).toBe(undefined);
+    done();
+  });
 });
+
