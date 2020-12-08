994336f80cbcf150ea0bb03de157c2948708c7d4
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-08 14:25:36 +0100

    Theme switcher component created

---
+++ src/components/UI/Theme/Theme.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { gaEvent } from "../../../utils/analytics.js";
+import "./Theme.scss";
+
+export default function Theme() {
+  const theme: any = localStorage.getItem("theme");
+  theme && document.documentElement.setAttribute("data-theme", theme);
+  useEffect(() => {
+    return () => { window.clearTimeout() };
+  }, []);
+  const isDarkTheme = () => {
+    if (theme === "dark") { return true }
+    if (theme === "light") { return false }
+  };
+  const checkbox = document.querySelector("input[name=theme-switcher]");
+  const trans = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("transition");
+    }, 1000);
+  };
+  checkbox &&
+    checkbox.addEventListener("change", (e: any) => {
+      if (e.target.checked) {
+        localStorage.setItem("theme", "dark");
+        trans();
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+        trans();
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+    });
+  const gaClickThemeSwitcher = () => {
+    gaEvent("click_theme_switcher", "clicks", "UI Clicks", true);
+  };
+  return (
+    <div className="layout__theme-switch">
+      <input
+        type="checkbox"
+        id="theme-switcher"
+        name="theme-switcher"
+        defaultChecked={isDarkTheme()}
+      />
+      <label htmlFor="theme-switcher" onClick={() => gaClickThemeSwitcher()}>
+        Toggle
+      </label>
+    </div>
+  );
+}
