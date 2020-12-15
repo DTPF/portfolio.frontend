136e569c851e55562fd73a4d05fc3ce4b08198d7
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-15 17:52:23 +0100

    useEffect added

--- src/webSockets/hooks/useMessagesUnreadLength.tsx
+++ src/webSockets/hooks/useMessagesUnreadLength.tsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import { ENDPOINT } from "../../config/url";
 
 export default function useMessagesUnreadLength() {
-  const [messagesUnreadLength, setMessagesUnreadLength] = useState(undefined);
-  const socket = socketIOClient(ENDPOINT);
-  socket.on("getMessagesLength", (data: any) => {
-    setMessagesUnreadLength(data);
-  });  
-  return [messagesUnreadLength];
+  const [messagesUnreadLength, setMessagesUnreadLength] = useState(null);
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("getMessagesLength", (data: any) => {
+      setMessagesUnreadLength(data);
+    });
+    return () => { socket.disconnect() };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return messagesUnreadLength;
 }
