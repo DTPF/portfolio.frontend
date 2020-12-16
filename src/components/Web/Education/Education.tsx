dc6313b28597478c34071019cd8e6cb43b728f0b
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-16 18:48:40 +0100

    Spin removed

--- src/components/Web/Education/Education.tsx
+++ src/components/Web/Education/Education.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import { useGetCourses } from "../../../hooks/useGetCourses";
 import useScrollToTop from "../../../hooks/useScrollToTop";
 import { useParams } from "react-router-dom";
@@ -6,9 +6,10 @@ import { Row, Col } from "antd";
 import "./Education.scss";
 import Courses from "../../../components/Web/Education/Courses";
 import InfoBanner from "../../../components/Web/Education/InfoBanner";
-import Spin from "../../../components/UI/Spin";
 const Pagination = lazy(() => import("../../../components/UI/Pagination"));
-const CourseInfo = lazy(() => import("../../../components/Web/Education/CourseInfo"));
+const CourseInfo = lazy(
+  () => import("../../../components/Web/Education/CourseInfo")
+);
 
 export default function Education(props: any) {
   const { location, history } = props;
@@ -28,14 +29,6 @@ export default function Education(props: any) {
 
 function RenderEducation(props: any) {
   const { url, location, history, courses } = props;
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    let unmounted = false;
-    if (!unmounted) {
-      setIsLoading(true);
-    }
-    return () => { unmounted = true };
-  }, []);
   const title = "Todos los cursos";
   const subtitle =
     "Cursos realizados online y " +
@@ -43,45 +36,39 @@ function RenderEducation(props: any) {
   return (
     <>
       {!url ? (
-        <>
-          {!isLoading ? (
-            <Spin />
-          ) : (
-            <Row className="education">
-              <Col lg={1} />
-              <Col lg={22}>
-                <div className="div"></div>
-                <Col className="education__info-courses" key="courses">
-                  <InfoBanner />
-                  <Courses
-                    numItems={10000}
-                    title={title}
-                    subtitle={subtitle}
-                    courses={courses && courses.docs}
-                    location={location}
-                    history={history}
-                  />
+        <Row className="education">
+          <Col lg={1} />
+          <Col lg={22}>
+            <div className="div"></div>
+            <Col className="education__info-courses" key="courses">
+              <InfoBanner />
+              <Courses
+                numItems={10000}
+                title={title}
+                subtitle={subtitle}
+                courses={courses && courses.docs}
+                location={location}
+                history={history}
+              />
+            </Col>
+            <div className="div"></div>
+            {courses?.totalPages > 1 && (
+              <>
+                <Col key="pagination">
+                  <Suspense fallback={<></>}>
+                    <Pagination
+                      courses={courses}
+                      location={location}
+                      history={history}
+                    />
+                  </Suspense>
                 </Col>
-                <div className="div"></div>
-                {courses?.totalPages > 1 && (
-                  <>
-                    <Col key="pagination">
-                      <Suspense fallback={<></>}>
-                        <Pagination
-                          courses={courses}
-                          location={location}
-                          history={history}
-                        />
-                      </Suspense>
-                    </Col>
-                  </>
-                )}
-                <div className="div"></div>
-              </Col>
-              <Col lg={1} />
-            </Row>
-          )}
-        </>
+              </>
+            )}
+            <div className="div"></div>
+          </Col>
+          <Col lg={1} />
+        </Row>
       ) : (
         <Suspense fallback={<></>}>
           <Col key={url}>
