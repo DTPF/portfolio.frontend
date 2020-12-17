619630967fa45fa492f07b9b7dc0b2d13120203c
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-17 17:57:32 +0100

    Hook added to reload courses when connection returns

--- src/hooks/useGetCourses.js
+++ src/hooks/useGetCourses.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import { getCoursesApi } from "../api/education";
 import queryString from "query-string";
+import { useDBConnectionStatus, useIsNavigatorOnline } from "../hooks/useConnection";
 
-export function useGetCourses(num, location) {
+export function useGetCourses(itemsPerPage, location = 1) {
   const { page = 1 } = queryString.parse(location.search);
   const [courses, setCourses] = useState([]);
+  const isNavigatorOnline = useIsNavigatorOnline();
+  const connectionStatus = useDBConnectionStatus();
   useEffect(() => {
     let isMounted = true;
-    getCoursesApi(num, page).then((response) => {
+    getCoursesApi(itemsPerPage, page).then((response) => {
       isMounted && setCourses(response.courses);
     });
     return () => { isMounted = false };
-  }, [num, page]);
-  return [courses];
+  }, [itemsPerPage, page, isNavigatorOnline, connectionStatus]);
+  return courses;
 }
