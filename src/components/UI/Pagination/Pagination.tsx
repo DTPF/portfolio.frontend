6596a67ba77c86967fe376b9a5e1c544ad246f1f
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-18 21:07:13 +0100

    Pagination improved and styles

--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Pagination as PaginationAntd } from "antd";
-
+import useScrollToTop from "../../../hooks/useScrollToTop";
 import "./Pagination.scss";
 
 export default function Pagination(props: any) {
-  const { courses, location, history } = props;
+  const { courses, location, history, itemsPerPage, setItemsPerPage } = props;
   const currentPage = parseInt(courses.page);
-  const onChangePage = (newPage: any) => {
+  const pageSize = courses.totalDocs;  
+  useScrollToTop(location);
+  const onChangePage = (newPage: number) => {
     history.push(`${location.pathname}?page=${newPage}`);
   };
+  const onShowSizeChange = (current: any, pageSize: any) => {
+    setItemsPerPage(pageSize);
+  }
   return (
     <PaginationAntd
       responsive
       defaultCurrent={currentPage}
       total={courses.totalDocs}
-      pageSize={courses.limit}
+      pageSize={itemsPerPage}
+      pageSizeOptions={["10", pageSize]}
+      onShowSizeChange={onShowSizeChange}
       onChange={newPage => onChangePage(newPage)}
       className="pagination"
     />
