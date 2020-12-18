fd0a47679c1a378688c0ef6daa89aa46f64ee311
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-18 17:59:24 +0100

    Component removed

--- src/components/Web/Curriculum/Curriculum.tsx
+++ src/components/Web/Curriculum/Curriculum.tsx
@@ -5,43 +5,39 @@ import Education from "../../../components/Web/Curriculum/Education";
 import ComplementaryEducation from "../../../components/Web/Curriculum/ComplementaryEducation";
 import ComputerSkills from "../../../components/Web/Curriculum/ComputerSkills";
 import Languages from "../../../components/Web/Curriculum/Languages";
-import Projects from "../../../components/Web/Curriculum/Projects";
+import useScrollToTop from "../../../hooks/useScrollToTop";
 import "./Curriculum.scss";
 
 export default function Curriculum() {
+  useScrollToTop();
   return (
-    <>
-      <div className="curriculum">
-        <Row>
-          <Col span={24}>
-            <h1 className="curriculum__title">CURRICULUM VITAE</h1>
-          </Col>
-          <Col span={24} xl={12}>
-            <WorkExperience />
-          </Col>
-          <Col span={24} xl={12}>
-            <Row>
-              <Col span={24} lg={12} xl={24}>
-                <Education />
-              </Col>
-              <Col span={24} lg={12} xl={24}>
-                <ComplementaryEducation />
-              </Col>
-            </Row>
-            <Col span={24}>
-              <ComputerSkills />
+    <div className="curriculum">
+      <Row>
+        <Col span={24}>
+          <h1 className="curriculum__title">CURRICULUM VITAE</h1>
+        </Col>
+        <Col span={24} xl={12}>
+          <WorkExperience />
+        </Col>
+        <Col span={24} xl={12}>
+          <Row>
+            <Col span={24} lg={12} xl={24}>
+              <Education />
             </Col>
-            <Row className="curriculum__languages-projects">
-              <Col span={24} md={10}>
-                <Languages />
-              </Col>
-              <Col span={24} md={14}>
-                <Projects />
-              </Col>
-            </Row>
+            <Col span={24} lg={12} xl={24}>
+              <ComplementaryEducation />
+            </Col>
+          </Row>
+          <Col span={24}>
+            <ComputerSkills />
           </Col>
-        </Row>
-      </div>
-    </>
+          <Row className="curriculum__languages-projects">
+            <Col span={24}>
+              <Languages />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
