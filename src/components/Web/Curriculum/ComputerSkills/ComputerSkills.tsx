b47b34e68540c16e717703d5f06a2f5a6a8e250d
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-18 18:03:12 +0100

    Renewed component

--- src/components/Web/Curriculum/ComputerSkills/ComputerSkills.tsx
+++ src/components/Web/Curriculum/ComputerSkills/ComputerSkills.tsx
@@ -5,61 +5,67 @@ import "./ComputerSkills.scss";
 
 export default function ComputerSkills() {
   const [show, el] = useNearScreen();
-  let ref: any = el;
+  const ref: any = el;
   return (
     <div className="computer-skills">
       <div className="use-near-screen" ref={ref}>
-      {show && (
-        <>
-          <h2>Competencias Informáticas</h2>
-          <Row>
-            <Col span={24} sm={12} className="computer-skills__col">
-              <Skill skill="HTML5" percent={70} steps={6} />
-              <Skill skill="CSS3" percent={70} steps={6} />
-              <Skill skill="SASS" percent={70} steps={6} />
-              <Skill skill="Bootstrap" percent={80} steps={6} />
-              <Skill skill="Ant-Design" percent={80} steps={6} />
-              <Skill skill="Javascript" percent={60} steps={6} />
-              <Skill skill="TypeScript" percent={40} steps={6} />
-              <Skill skill="NodeJS" percent={40} steps={6} />
-              <Skill skill="ExpressJS" percent={60} steps={6} />
-              <Skill skill="React" percent={70} steps={6} />
-              <Skill skill="Angular" percent={40} steps={6} />
-              <Skill skill="Vue" percent={40} steps={6} />
-              <Skill skill="JQuery" percent={20} steps={6} />
-              <Skill skill="Ajax" percent={60} steps={6} />
-            </Col>
-            <Col span={24} sm={12} className="computer-skills__col">
-              <Skill skill="Axios" percent={40} steps={6} />
-              <Skill skill="Xampp, Wamp" percent={60} steps={6} />
-              <Skill skill="Vagrant" percent={60} steps={6} />
-              <Skill skill="PHP" percent={40} steps={6} />
-              <Skill skill="Laravel" percent={60} steps={6} />
-              <Skill skill="Symfony" percent={20} steps={6} />
-              <Skill skill="Nginx" percent={60} steps={6} />
-              <Skill skill="MongoDB" percent={40} steps={6} />
-              <Skill skill="MySQL" percent={40} steps={6} />
-              <Skill skill="MariaDB" percent={40} steps={6} />
-              <Skill skill="Git" percent={60} steps={6} />
-              <Skill skill="Linux" percent={70} steps={6} />
-              <Skill skill="Windows" percent={60} steps={6} />
-              <Skill skill="Wordpress" percent={60} steps={6} />
-            </Col>
-          </Row>
-        </>
-      )}
-    </div>
+        {show && (
+          <>
+            <h2>Competencias Informáticas</h2>
+            <Row>
+              <Col span={24} sm={12} className="computer-skills__col">
+                <Skill skill="HTML5" percent={70} status="active" />
+                <Skill skill="CSS3" percent={70} status="active" />
+                <Skill skill="SASS" percent={70} status="active" />
+                <Skill skill="Bootstrap" percent={85} status="active" />
+                <Skill skill="Ant-Design" percent={85} status="active" />
+                <Skill skill="Javascript" percent={60} status="active" />
+                <Skill skill="TypeScript" percent={40} status="active" />
+                <Skill skill="NodeJS" percent={40} status="active" />
+                <Skill skill="ExpressJS" percent={60} status="active" />
+                <Skill skill="React" percent={70} status="active" />
+                <Skill skill="Angular" percent={40} />
+                <Skill skill="Vue" percent={40} />
+                <Skill skill="JQuery" percent={20} />
+              </Col>
+              <Col span={24} sm={12} className="computer-skills__col">
+                <Skill skill="Xampp, Wamp" percent={60} />
+                <Skill skill="Vagrant" percent={60} />
+                <Skill skill="PHP" percent={40} status="active" />
+                <Skill skill="Laravel" percent={60} status="active" />
+                <Skill skill="Symfony" percent={20} />
+                <Skill skill="Nginx" percent={60} status="active" />
+                <Skill skill="MongoDB" percent={40} status="active" />
+                <Skill skill="MySQL" percent={40} />
+                <Skill skill="MariaDB" percent={40} status="active" />
+                <Skill skill="Git" percent={50} status="active" />
+                <Skill skill="Linux" percent={70} status="active" />
+                <Skill skill="Windows" percent={50} />
+                <Skill skill="Wordpress" percent={50} />
+              </Col>
+            </Row>
+          </>
+        )}
+      </div>
     </div>
   );
 }
 
 function Skill(props: any) {
-  const { percent, steps, skill } = props;
+  const { skill, percent, status = "normal" } = props;
   return (
     <div className="computer-skills__skill">
       {skill}
-      <span>
-        <Progress percent={percent} steps={steps} showInfo={false} />
+      <span className="computer-skills__skill-progress">
+        <Progress
+          strokeColor={{
+            from: "#5d718d",
+            to: "#273b56",
+          }}
+          percent={percent}
+          status={status}
+          showInfo={false}
+        />
       </span>
     </div>
   );
