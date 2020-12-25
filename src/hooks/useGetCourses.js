e5442eff99cfd883aafa1ca8247fecf71f7ca7e8
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-25 12:28:28 +0100

    Refactored

--- src/hooks/useGetCourses.js
+++ src/hooks/useGetCourses.js
@@ -3,16 +3,16 @@ import { getCoursesApi } from "../api/education";
 import queryString from "query-string";
 import useConnection from "./useConnection";
 
-export function useGetCourses(itemsPerPage, location = 1) {
-  const { page = 1 } = queryString.parse(location.search);
+export function useGetCourses(itemsPerPage, location) {
   const [courses, setCourses] = useState([]);
   const { connectionStatus, isNavigatorOnline } = useConnection();
+  const { page = 1 } = queryString.parse(location.search);
   useEffect(() => {
     let isMounted = true;
-    getCoursesApi(itemsPerPage, page).then((response) => {
-      isMounted && setCourses(response.courses);
-    });
+      getCoursesApi(itemsPerPage, page).then((response) => {
+        isMounted && setCourses(response.courses);
+      });    
     return () => { isMounted = false };
-  }, [itemsPerPage, page, isNavigatorOnline, connectionStatus]);
+  }, [itemsPerPage, page, connectionStatus, isNavigatorOnline]);
   return courses;
 }
