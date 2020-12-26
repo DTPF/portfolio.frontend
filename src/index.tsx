7e4f4517565a0fdd150be81c3c40acdad1dd2095
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-26 12:06:07 +0100

    reportWebVitals added

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.min.css";
 import "./index.scss";
 import App from "./App";
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// reportWebVitals(console.log);
