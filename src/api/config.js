3574c44af9178ef05c786cbeafd52471d709ff62
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-26 11:20:41 +0100

    Comments removed

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,3 @@
 import { isLocalhost } from "../utils/isLocalhost";
-export const basePath = isLocalhost ? "http://localhost:3998/api" : "https://dtpf.es/api";
-// export const basePath = "https://beta.dtpf.es/api";
+export const basePath = isLocalhost ? "http://localhost:3998/api" : "https://beta.dtpf.es/api";
 export const apiVersion = "v1";
--- src/config/url.js
+++ src/config/url.js
@@ -1,5 +1,3 @@
 import { isLocalhost } from "../utils/isLocalhost";
-export const URL = isLocalhost ? "http://localhost:3000/" : "https://dtpf.es/";
-export const ENDPOINT = isLocalhost ? "http://localhost:3998" : "https://dtpf.es";
-// export const URL = "https://beta.dtpf.es/";
-// export const ENDPOINT = "https://beta.dtpf.es";
+export const URL = isLocalhost ? "http://localhost:3000/" : "https://beta.dtpf.es/";
+export const ENDPOINT = isLocalhost ? "http://localhost:3998" : "https://beta.dtpf.es";
