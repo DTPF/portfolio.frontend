da5b19e6ac11a9467488aa4e622b36bf426a8f62
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-26 11:57:55 +0100

    Types modules declared

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+declare module "react/jsx-runtime" {
+  export default any;
+}
+declare module "react-cookie-consent" {
+  export default any;
+}
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.webp';
+declare module '*.mp3';
+declare module '*.ogg';
