3f50be5892fcc9da407e77523fa4579dffa63f9a
David Pizarro <davidpizarrofrick@gmail.com>
2020-12-30 17:33:29 +0100

    ServiceWorker registration added

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.min.css";
 import "./index.scss";
 import App from "./App";
-// import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { message } from "antd";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
 // reportWebVitals(console.log);
+serviceWorkerRegistration.register({
+  onUpdate: (registration: any) => {
+    const reload = () => window.location.reload();
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      registration.update();
+      message.info("Nueva versión. Actualizando la web", reload);
+    }
+  },
+});
