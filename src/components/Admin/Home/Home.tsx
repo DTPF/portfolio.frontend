44ce6962840af021c50d9ef192ca7adb4e7e863a
David Pizarro <davidpizarrofrick@gmail.com>
2021-01-02 12:55:29 +0100

    Logic added to update indexedDb

--- src/components/Admin/Home/Home.tsx
+++ src/components/Admin/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { checkUserLogin } from "../../../providers/AuthProvider";
 import { useHistory } from "react-router";
+import { checkUserLogin } from "../../../providers/AuthProvider";
+import {
+  getIndexedDBVersionApi,
+  updateIndexedDBVersionApi,
+} from "../../../api/utils";
 import { useCookies } from "react-cookie";
-import { Button } from "antd";
+import { Button, message, Modal } from "antd";
+import "./Home.scss";
 
 export default function Home(props: any) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -17,28 +22,78 @@ export default function Home(props: any) {
     }
     history.go(0);
   };
-  const [user, setUser] = useState(null);  
+  const [user, setUser] = useState(null);
   useEffect(() => {
     let isMounted = true;
     isMounted && checkUserLogin(setUser);
-    return () => { isMounted = false }
+    return () => {
+      isMounted = false;
+    };
   }, []);
-  return (
-    <RenderHome userData={user} deleteCookies={deleteCookies} />
-  );
+  return <RenderHome userData={user} deleteCookies={deleteCookies} />;
 }
 
 function RenderHome(props: any) {
   const { deleteCookies, userData } = props;
+  const [idbVersion, setIdbVersion] = useState(null);
+  const { confirm } = Modal;
+  useEffect(() => {
+    let isMounted = true;
+    getIndexedDBVersionApi().then((res) => {
+      res && isMounted && setIdbVersion(res.indexedDBVersion);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  const showConfirm = () => {
+    confirm({
+      title: "Actualizando IndexedDB",
+      content: `¿Estás seguro que quieres actualizar IndexedDB?`,
+      okText: "Actualizar",
+      okType: "primary",
+      cancelText: "Cancelar",
+      onOk() {
+        updateIndexedDBVersionApi()
+          .then((res) => {
+            if (res) {
+              if (res.status !== 200) {
+                message.warn(res.message);
+              } else {
+                message.success(res.message);
+                setIdbVersion(res.indexedDBVersion);
+              }
+            } else {
+              message.error("No hay respuesta al servidor.");
+            }
+          })
+          .catch((err) => {
+            message.error(err.message);
+          });
+      },
+    });
+  };
   return (
-    <>
-      <h1>
-        Hola {userData ? userData.user.name : "Anónimo"}{" "}
-        {userData && userData.user.lastname}
-      </h1>
-      <Button type="primary" onClick={deleteCookies}>
-        Borrar Cookies
-      </Button>
-    </>
+    <div className="admin-home">
+      <div className="admin-home__title">
+        <h1>
+          {`Hola ${userData ? userData.user.name : "Anónimo"}
+        ${userData && userData.user.lastname}`}
+        </h1>
+      </div>
+      <div className="admin-home__cookies">
+        <Button type="primary" onClick={deleteCookies}>
+          Borrar Cookies
+        </Button>
+      </div>
+      <div className="admin-home__idb">
+        <h1>
+          IndexedDB Version <span>{idbVersion && idbVersion}</span>
+        </h1>
+        <Button type="primary" onClick={showConfirm}>
+          Actulizar IndexedDB
+        </Button>
+      </div>
+    </div>
   );
 }
