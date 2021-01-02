3b917fb3f192cfb167c1f83149a83db95f7238ed
David Pizarro <davidpizarrofrick@gmail.com>
2021-01-02 12:33:45 +0100

    Offline menu added

--- src/components/Web/Layout/MenuSider/MenuSider.tsx
+++ src/components/Web/Layout/MenuSider/MenuSider.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import useGetMainMenu from "../../../../hooks/useGetMainMenu";
+import GetMainMenu from "../../../../dbIndexed/mainMenu/GetMainMenu";
 import { Layout, Menu } from "antd";
 import "./MenuSider.scss";
 import SocialLinks from "../../SocialLinks";
 
 function MenuSider(props: any) {
   const { menuCollapsed, setMenuCollapsed, location } = props;
-  const { mainMenu } = useGetMainMenu();
+  const menu = GetMainMenu();
+  return (
+    <RenderMenuSider
+      menuCollapsed={menuCollapsed}
+      setMenuCollapsed={setMenuCollapsed}
+      location={location}
+      mainMenu={menu}
+    />
+  );
+}
+
+function RenderMenuSider(props: any) {
+  const { menuCollapsed, setMenuCollapsed, location, mainMenu } = props;
   const { Sider } = Layout;
   return (
     <Sider
@@ -18,24 +30,25 @@ function MenuSider(props: any) {
       collapsed={menuCollapsed}
       onClick={() => setMenuCollapsed(!menuCollapsed)}
     >
-      <Menu selectedKeys={[location.pathname]}  mode="vertical">
-        {mainMenu.map((item: any) => {
-          const external = item.url.indexOf("http") > -1 ? true : false;
-          if (external) {
+      <Menu selectedKeys={[location.pathname]} mode="vertical">
+        {mainMenu &&
+          mainMenu.map((item: any) => {
+            const external = item.url.indexOf("http") > -1 ? true : false;
+            if (external) {
+              return (
+                <Menu.Item key={item.url} className={"menu-sider-web__item"}>
+                  <a href={item.url} target="_blank" rel="noopener noreferrer">
+                    {item.title}
+                  </a>
+                </Menu.Item>
+              );
+            }
             return (
               <Menu.Item key={item.url} className={"menu-sider-web__item"}>
-                <a href={item.url} target="_blank" rel="noopener noreferrer">
-                  {item.title}
-                </a>
+                <Link to={item.url}>{item.title}</Link>
               </Menu.Item>
             );
-          }
-          return (
-            <Menu.Item key={item.url} className={"menu-sider-web__item"}>
-              <Link to={item.url}>{item.title}</Link>
-            </Menu.Item>
-          );
-        })}
+          })}
         <div className="menu-sider-web__item-social-sider">
           <SocialLinks />
         </div>
--- src/components/Web/Layout/MenuTop/MenuTop.tsx
+++ src/components/Web/Layout/MenuTop/MenuTop.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import useGetMainMenu from "../../../../hooks/useGetMainMenu";
+import GetMainMenu from "../../../../dbIndexed/mainMenu/GetMainMenu";
 import { gaEvent } from "../../../../utils/analytics.js";
 import { Menu, Button } from "antd";
 import Logo from "../../../../assets/img/png/logo128.png";
-import {
-  MenuOutlined,
-  CloseCircleOutlined
-} from "@ant-design/icons";
+import { MenuOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import "./MenuTop.scss";
 import SocialLinks from "../../SocialLinks";
 
 function MenuTop(props: any) {
-  const { mainMenu } = useGetMainMenu();
   const { menuCollapsed, setMenuCollapsed, location } = props;
+  const menu = GetMainMenu();
+  return (
+    <RenderMenu
+      menuCollapsed={menuCollapsed}
+      setMenuCollapsed={setMenuCollapsed}
+      location={location}
+      mainMenu={menu}
+    />
+  );
+}
+
+function RenderMenu(props: any) {
+  const { menuCollapsed, setMenuCollapsed, location, mainMenu } = props;
   const clickMenuIcon = () => {
     gaEvent("click_menu_mobile_icon", "clicks", "UI Clicks", true);
-  };  
+  };
   return (
     <Menu
       selectedKeys={[location.pathname]}
@@ -24,27 +33,28 @@ function MenuTop(props: any) {
       mode="horizontal"
     >
       <Menu.Item className="menu-top-web__logo">
-        <NavLink to={"/"} >
+        <NavLink to={"/"}>
           <img src={Logo} alt="Logo de David Thomas Pizarro Frick" />
         </NavLink>
       </Menu.Item>
-      {mainMenu.map((item: any) => {
-        const external = item.url.indexOf("http") > -1 ? true : false;
-        if (external) {
+      {mainMenu &&
+        mainMenu.map((item: any) => {
+          const external = item.url.indexOf("http") > -1 ? true : false;
+          if (external) {
+            return (
+              <Menu.Item key={item.url} className="menu-top-web__item">
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  {item.title}
+                </a>
+              </Menu.Item>
+            );
+          }
           return (
             <Menu.Item key={item.url} className="menu-top-web__item">
-              <a href={item.url} target="_blank" rel="noopener noreferrer">
-                {item.title}
-              </a>
+              <NavLink to={item.url}>{item.title}</NavLink>
             </Menu.Item>
           );
-        }
-        return (
-          <Menu.Item key={item.url} className="menu-top-web__item">
-            <NavLink to={item.url}>{item.title}</NavLink>
-          </Menu.Item>
-        );
-      })}
+        })}
       <div>
         <div className="menu-top-web__item-social">
           <SocialLinks />
@@ -53,7 +63,10 @@ function MenuTop(props: any) {
           <Button
             type="link"
             className="menu-top-web__menu"
-            onClick={() => {clickMenuIcon(); setMenuCollapsed(!menuCollapsed)}}
+            onClick={() => {
+              clickMenuIcon();
+              setMenuCollapsed(!menuCollapsed);
+            }}
           >
             {React.createElement(
               menuCollapsed ? MenuOutlined : CloseCircleOutlined
