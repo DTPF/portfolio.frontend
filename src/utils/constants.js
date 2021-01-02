074c24eecc822f3dac5c3f1577640214719f3f04
David Pizarro <davidpizarrofrick@gmail.com>
2021-01-02 12:19:09 +0100

    window.Storage constants added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
 export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
 export const testingRefreshToken = "";
+
+// window.Storage
+export const COURSES_TECH = "c_tech";
+export const COURSES_HOURS = "c_hour";
