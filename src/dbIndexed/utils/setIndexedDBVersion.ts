b5fa5426f2f4efe116fbbd82b0ede81aab8894a2
David Pizarro <davidpizarrofrick@gmail.com>
2021-01-06 14:22:54 +0100

    Setting first version

--- src/dbIndexed/utils/setIndexedDBVersion.ts
+++ src/dbIndexed/utils/setIndexedDBVersion.ts
@@ -5,16 +5,15 @@ import { message } from "antd";
 
 export default function setIndexedDBVersion(db: any) {
   setIdbVersion();
-  setInterval(() => {
-    setIdbVersion();
-  }, 60000);
+  const ls = localStorage.getItem(DB_DTPF_LS_NAME);
   function setIdbVersion() {
     getIndexedDBVersionApi()
       .then((res) => {
-        res.indexedDBVersion && StorageValid() &&
-          localStorage.setItem(DB_DTPF_LS_NAME, res.indexedDBVersion);
-        const reload = () => window.location.reload();
+        res.indexedDBVersion &&
+          StorageValid() &&
+          localStorage.setItem(DB_DTPF_LS_NAME, ls ? res.indexedDBVersion : 1);
         if (res.indexedDBVersion === db.version + 1) {
+          const reload = () => window.location.reload();
           message.info("Contenido nuevo. Actualizando la web", reload);
         }
       })
